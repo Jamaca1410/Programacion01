ff10dd2dc0165ce5eb67a76b04aab2c7568e0cbe
Jamaca1410 <jamaca1410@gmail.com>
2021-05-10 21:07:25 -0400

    Clases empleado agregada

--- Ejercicio 1/Cuenta Bancaria/Program.cs
+++ Ejercicio 1/Cuenta Bancaria/Program.cs
@@ -2,26 +2,46 @@
 
 namespace Cuenta_Bancaria
 {
-    public class Cuenta 
+    public class CuentaB
     {
-        public Double Balance = 9000000;
+        public double Balance = 9000000;
         public int NoCuenta = 708117810;
         public string Nombre = "Juan";
         public string Apellido = "Perez";
         public string FechVencimiento = "5/27";
 
     }
-    public class Program
+ 
+    public class empleado
     {
-       public static void Main()
+        public string nombre = "Marco";
+        public string apellido = "Vargas";
+        public double Sueldo = 15000.00;
+        public char sexo = 'M';
+        public string cargo = "Analista de datos";
+        public long cedula = 40212345868;
+
+    }
+    public class program
+    {
+        public static void Main()
         {
-            Cuenta objcuenta = new Cuenta();
-            Console.WriteLine("Nombre =" + objcuenta.Nombre);
-            Console.WriteLine("Apellido =" + objcuenta.Apellido);
-            Console.WriteLine("No. Cuenta =" + objcuenta.NoCuenta);
-            Console.WriteLine("Balance =" + objcuenta.Balance);
-            Console.WriteLine("Fecha de Vencimiento =" + objcuenta.FechVencimiento);
+            Console.WriteLine("Clases Cuanta Bancaria: ");
+            CuentaB objcuentaB = new CuentaB();
+            Console.WriteLine("Nombre =" + objcuentaB.Nombre);
+            Console.WriteLine("Apellido =" + objcuentaB.Apellido);
+            Console.WriteLine("No. Cuenta =" + objcuentaB.NoCuenta);
+            Console.WriteLine("Balance =" + objcuentaB.Balance);
+            Console.WriteLine("Fecha de Vencimiento =" + objcuentaB.FechVencimiento);
+            Console.WriteLine("\n Clases Empleado: ");
+            empleado objempleado = new empleado();
+            Console.WriteLine("Nombre = " + objempleado.nombre);
+            Console.WriteLine("Apellido = " + objempleado.apellido);
+            Console.WriteLine("Sueldo = " + objempleado.Sueldo);
+            Console.WriteLine("Sexo = " + objempleado.sexo);
+            Console.WriteLine("Cargo = " + objempleado.cargo);
+            Console.WriteLine("Cédula = " + objempleado.cedula);
         }
     }
-    
+
 }
