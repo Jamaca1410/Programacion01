b4c3c735c3151c94434164c10ad6a56e9aabcf2c
Jamaca1410 <jamaca1410@gmail.com>
2021-06-26 17:46:49 -0400

    Cambios

---
+++ Bonos/Ejr Bono poli/Ejercicio Poli BONO/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Ejercicio_Poli_BONO
+{
+    public class Cuenta
+    {
+        int cuenta;
+        string nombre;
+        double saldo;
+
+        public void consulta()
+        {
+
+        }
+
+        public double Deposito(double dep)
+        {
+
+            return dep;
+        }
+        public double Retiro(double ret)
+        {
+
+            return ret;
+        }
+
+    }
+
+    public class Cahorro : Cuenta
+    {
+        int dia, mes, año;
+        float intmensual;
+
+        if (dia==1)
+            {
+            }
+
+• Solamente se puede retirar dinero el día de la fecha de vencimiento
+
+    }
+
+    public class Ccheques : Cuenta
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Parcia2/Ejercicio1/JavierCarrillo 10138957 grupo 1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace JavierCarrillo_10138957_grupo_1
+{
+    public class Publicacion
+    {
+        public string titulo;
+        public int precio;
+        public Publicacion()
+        {
+            Console.WriteLine("Ingrese el titulo:");
+            titulo = Console.ReadLine();
+            Console.WriteLine("Ingrese el precio:");
+            precio = int.Parse(Console.ReadLine());
+        }
+ 
+        public void MostrarPublicacion()
+        {
+            Console.WriteLine("Titulo: {0}", titulo);
+            Console.WriteLine("Precio: {0}", precio);
+        }
+    }
+    sealed class Libro : Publicacion
+    {
+        public int paginas;
+        public Libro() 
+        {
+            this.paginas = paginas;
+            Console.WriteLine("Ingrese el numero de paginas:");
+            paginas = int.Parse(Console.ReadLine());
+        }
+  
+        public void Mostrarlibro()
+        {            
+            Console.WriteLine("Paginas: {0} \n", paginas);
+        }
+    }
+     sealed class CD : Publicacion
+    {
+        public float tiempo;
+        public CD() 
+        {
+            Console.WriteLine("Ingrese la duracion del CD en minutos:");
+            tiempo = float.Parse(Console.ReadLine());
+        }
+
+        public void MostrarCD()
+        {
+            Console.WriteLine("Tiempo de duracion: {0} \n", tiempo);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char flag;
+            do
+            {
+                Libro objlibro = new Libro();
+                objlibro.MostrarPublicacion();
+                objlibro.Mostrarlibro();
+                CD objCD = new CD();
+                objCD.MostrarPublicacion();
+                objCD.MostrarCD();
+
+            flag:
+                Console.WriteLine("Desea ingresar mas datos? (y/n)");
+                try
+                {
+                    if()
+                    {
+                        flag = char.Parse(Console.ReadLine());
+                    }   
+                }
+                
+                catch
+                {
+                    Console.WriteLine("Input invalido");
+                    goto flag;
+                }
+                Console.Clear();
+            } while (flag != 'n' && flag != 'N');
+            
+           
+        }
+    }
+}
