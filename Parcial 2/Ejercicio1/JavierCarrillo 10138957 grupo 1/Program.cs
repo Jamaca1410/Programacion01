32159eb36329ef37f8da83d7f3cd97abba607d96
Jamaca1410 <jamaca1410@gmail.com>
2021-06-27 14:00:57 -0400

    PARCIAL 2 ACABADO REVISAR FALLOS

--- Parcial 2/Ejercicio1/JavierCarrillo 10138957 grupo 1/Program.cs
+++ Parcial 2/Ejercicio1/JavierCarrillo 10138957 grupo 1/Program.cs
@@ -5,34 +5,64 @@ namespace JavierCarrillo_10138957_grupo_1
     public class Publicacion
     {
         public string titulo;
-        public int precio;
+        public float precio;
         public Publicacion()
         {
             Console.WriteLine("Ingrese el titulo:");
             titulo = Console.ReadLine();
-            Console.WriteLine("Ingrese el precio:");
-            precio = int.Parse(Console.ReadLine());
+        precio11: try
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese el precio:");
+                    precio = float.Parse(Console.ReadLine());
+                    if (precio < 1)
+                    {
+                        Console.WriteLine("El precio debe ser un numero positivo.");
+                    }
+                } while (precio < 0);
+            }
+            catch
+            {
+                Console.WriteLine("input invalido.");
+                goto precio11;
+            }
+
         }
  
         public void MostrarPublicacion()
         {
-            Console.WriteLine("Titulo: {0}", titulo);
-            Console.WriteLine("Precio: {0}", precio);
+            Console.WriteLine("Titulo: {0}.", titulo);
+            Console.WriteLine("Precio: ${0} PESOS DOMINICANOS.", precio);
         }
     }
     sealed class Libro : Publicacion
     {
         public int paginas;
         public Libro() 
-        {
-            this.paginas = paginas;
-            Console.WriteLine("Ingrese el numero de paginas:");
-            paginas = int.Parse(Console.ReadLine());
+        {     
+            pag: try
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese el numero de paginas:");
+                    paginas = int.Parse(Console.ReadLine());
+                    if (paginas < 1)
+                    {
+                        Console.WriteLine("El numero de paginas debe ser positivo.");
+                    }
+                } while (paginas < 1);
+            }
+            catch
+            {
+                Console.WriteLine("input invalido.");
+                goto pag;
+            }
         }
   
         public void Mostrarlibro()
         {            
-            Console.WriteLine("Paginas: {0} \n", paginas);
+            Console.WriteLine("Paginas: {0}. \n", paginas);
         }
     }
      sealed class CD : Publicacion
@@ -40,13 +70,27 @@ namespace JavierCarrillo_10138957_grupo_1
         public float tiempo;
         public CD() 
         {
-            Console.WriteLine("Ingrese la duracion del CD en minutos:");
-            tiempo = float.Parse(Console.ReadLine());
+        time: try
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese la duracion del CD en minutos:");
+                    tiempo = float.Parse(Console.ReadLine());
+                    if (tiempo < 1)
+                    {
+                        Console.WriteLine("El tiempo debe ser un numero positivo.");
+                    }
+                } while (tiempo < 1);
+            }
+            catch
+            {
+                Console.WriteLine("input invalido.");
+                goto time;
+            }            
         }
-
         public void MostrarCD()
         {
-            Console.WriteLine("Tiempo de duracion: {0} \n", tiempo);
+            Console.WriteLine("Tiempo de duracion: {0} minutos.\n", tiempo);
         }
 
     }
@@ -57,32 +101,42 @@ namespace JavierCarrillo_10138957_grupo_1
             char flag;
             do
             {
+                Console.WriteLine("INGRESE LOS DATOS DEL LIBRO FÍSICO: \n");
                 Libro objlibro = new Libro();
+                Console.Clear();
+                Console.WriteLine("LIBRO FÍSICO: \n");
                 objlibro.MostrarPublicacion();
                 objlibro.Mostrarlibro();
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("INGRESE LOS DATOS DEL LIBRO AUDIO: \n");
                 CD objCD = new CD();
+                Console.Clear();
+                Console.WriteLine("LIBRO CD: \n");
                 objCD.MostrarPublicacion();
                 objCD.MostrarCD();
+                Console.ReadKey();
+                Console.Clear();
 
-            flag:
-                Console.WriteLine("Desea ingresar mas datos? (y/n)");
-                try
+            flag1: try
                 {
-                    if()
+                    do
                     {
+                        Console.WriteLine("Desea ingresar mas datos? (S/n)");
                         flag = char.Parse(Console.ReadLine());
-                    }   
+                        if (flag != 'n' && flag != 'N' && flag != 's' && flag != 'S')
+                        {
+                            Console.WriteLine("El caracter ingresado no es valido.");
+                        }
+                    } while (flag != 'n' && flag != 'N' && flag != 's' && flag != 'S');
                 }
-                
                 catch
                 {
-                    Console.WriteLine("Input invalido");
-                    goto flag;
-                }
+                    Console.WriteLine("input invalido.");
+                    goto flag1;
+                }                                        
                 Console.Clear();
-            } while (flag != 'n' && flag != 'N');
-            
-           
+            } while (flag != 'n' && flag != 'N');                       
         }
     }
 }
