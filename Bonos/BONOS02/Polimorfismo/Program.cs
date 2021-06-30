b04ab39824ae67556ccf1468badb729636d6a070
Jamaca1410 <jamaca1410@gmail.com>
2021-06-30 13:29:32 -0400

    ejr 2 poli

---
+++ Bonos/BONOS02/Polimorfismo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Polimorfismo
+{
+    public abstract class Animal
+    {
+        public int peso;
+        public Animal()
+        {
+            do
+            {
+            an: try
+                {
+                    Console.WriteLine("Ingrese el peso del animal en kilos:");
+                    peso = int.Parse(Console.ReadLine());
+                    if (peso < 1)
+                    {
+                        Console.WriteLine("El peso del animal debe ser un numero positivo.");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("El input ingresado es invalido.");
+                    goto an;
+                }                
+            } while(peso<1);
+            Console.WriteLine("El animal pesa: {0} Kilos.", peso);
+        }
+        public virtual string Dice(){ return ""; }
+    }
+
+    public class Vaca : Animal
+    {
+        public override string Dice() 
+        {
+            return "mu";
+        }
+    }
+
+    public class Serpiente : Animal
+    {
+        public override string Dice()
+        {
+            return "sss";
+        }
+    }
+
+    public class Cerdo : Animal
+    {
+        public override string Dice()
+        {
+            return "oinc";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vaca mascotaVaca = new Vaca(); 
+            Console.WriteLine(mascotaVaca.Dice());
+            Serpiente mascotaSerpiente = new Serpiente();      
+            Console.WriteLine(mascotaSerpiente.Dice());
+            Cerdo mascotaCerdo = new Cerdo();           
+            Console.WriteLine(mascotaCerdo.Dice());
+        }
+    }
+}
