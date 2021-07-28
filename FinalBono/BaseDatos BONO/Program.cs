5dd7513f4561e14da2d039902528e0d89482c389
Jamaca1410 <jamaca1410@gmail.com>
2021-07-28 15:39:11 -0400

    asdasd

---
+++ FinalBono/BaseDatos BONO/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+// Hay que tener instalado este paquete para que el cod. funcione
+using MySql.Data.MySqlClient;
+
+namespace FinalBono
+{
+    class Program
+    {
+        static string connection = "SERVER=localhost; PORT = 3306; uid = root; DATABASE=estudiantes; charset = utf8; sslMode = none";
+        static MySqlConnection sqlcon = new MySqlConnection(connection);
+        static void Main(string[] args)
+        {
+            
+            using (sqlcon)
+            {
+                inicio: try
+
+                {
+                    Console.WriteLine("Connectando a MySql");
+                    //entrada de datos
+                    Console.WriteLine("ID estudiante:");
+                    int Estudianteid = int.Parse(Console.ReadLine());
+
+
+                    Console.WriteLine("Nombre estudiante:");
+                    string Nombre = Console.ReadLine();
+                    if (Nombre == string.Empty || Nombre.Length > 25)
+                        goto inicio;
+                   
+                    Console.WriteLine("Apellido estudiante:");
+                    string Apellido = Console.ReadLine();
+                    if (Apellido == string.Empty || Apellido.Length > 25)
+                        goto inicio;
+
+                    Console.WriteLine("Sexo estudiante:");
+                    string Sexo = Console.ReadLine();
+                    if (Sexo == string.Empty || Sexo.Length > 1 || (Sexo != "M" && Sexo != "F" && Sexo != "m" && Sexo != "f"))
+                        goto inicio;
+
+                    Console.WriteLine("Direccion estudiante:");
+                    string Direccion = Console.ReadLine();
+                    if (Direccion == string.Empty || Direccion.Length > 15)
+                        goto inicio;
+
+                    string query = "insert into Estudiante values(" + Estudianteid + ", '" + Nombre + "', '" + Apellido + "', '" + Sexo + "', '" + Direccion + "') ";
+                    Console.WriteLine(query);
+                    //iniciar coneccion a base de datso
+                    sqlcon.Open();
+                    Console.WriteLine("Connection done " + sqlcon.State.ToString() + Environment.NewLine);
+                    //
+                    MySqlCommand cmd = new MySqlCommand(query, sqlcon);
+                    int result = cmd.ExecuteNonQuery();
+                    Console.WriteLine(result + "record/s inserted in Estudiante table");
+                    //
+                    MySqlCommand command = sqlcon.CreateCommand();
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "Select * from Estudiante";
+
+                    MySqlDataReader reader = command.ExecuteReader();
+                    var data = "[Id]\t[Nombre]\t[Apellido]\t[Sexo]\t[Direccion]\n";
+
+
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            data += reader.GetInt32(0) + "\t" + reader.GetString(1) + "\t\t" + reader.GetString(2) + "\t\t" + reader.GetString(3) + "\t" + reader.GetString(4) + Environment.NewLine;
+                        }
+                        Console.WriteLine(data);
+                    }
+                    else
+                    {
+                        Console.WriteLine("TABLA VACIA");
+                    }
+                    reader.Close();
+
+                    sqlcon.Close();
+                    Console.WriteLine("Connection done " + sqlcon.State.ToString() + Environment.NewLine);
+                }
+                catch (MySql.Data.MySqlClient.MySqlException ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message.ToString());
+                }
+                catch(FormatException ee)
+                {
+                    Console.WriteLine("Error: " + ee.Message);
+                    goto inicio;
+                }
+                finally
+                {
+                    Console.WriteLine("Presiona cualquier tecla...");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ Tarea02/Tarea 2/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class exepcionP : Exception
+{
+    public exepcionP(string str)
+    {
+       
+    }
+}
+
+namespace Tarea02
+{
+
+    public class Datos
+    {
+        public string asignatura, aula, instructor;
+        public int creditos;
+        public bool xd = true, zd =true;
+        public Datos()
+        {
+            Console.WriteLine("Ingrese el nombre de la asignatura:");
+            asignatura = Console.ReadLine();      
+            Console.WriteLine("Ingrese el aula de la asignatura:");
+            aula = Console.ReadLine();   
+
+            credi: try
+            {
+                Console.WriteLine("Ingrese los creditos de la asignatura:");
+                creditos = int.Parse(Console.ReadLine());
+                if (creditos < 0)
+                {
+                    throw new exepcionP("");
+                }
+                zd = true;
+            }
+            catch (exepcionP)
+            {
+                Console.WriteLine("Los creditos no pueden ser menor que 0.");
+                zd = false;
+                goto credi;
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("El input es invalido.");
+                zd = false;
+                goto credi;
+            }
+          
+            finally
+            {
+                if(creditos > -1 && zd == true)
+                Console.WriteLine("El dato fue regristrado");        
+            }
+            nom: try
+            {
+                Console.WriteLine("Ingrese el nombre del instructor:");
+                instructor = Console.ReadLine();
+                if (Regex.IsMatch(instructor, @"^[0-9]+$"))
+                {
+                    throw new exepcionP("");
+                }
+                xd = true;
+            }
+            catch(exepcionP)
+            {
+                Console.WriteLine("Los nombres no pueden ser numeros.");
+                xd = false;
+                goto nom;               
+            }
+            finally
+            {
+                if (xd == true)
+                {
+                    Console.Clear();
+                }       
+            }
+        }
+        public virtual void mostrarDatos()
+        {
+            Console.WriteLine("La asignatura es:" + asignatura);
+            Console.WriteLine("El aula es:" + aula);
+            Console.WriteLine("Los creditos de la asignatura son:" + creditos);
+        }
+    }
+    public class evaluacion : Datos
+    {
+        public override void mostrarDatos()
+        {
+            string path = @"C:\Users\Javier Carrillo\Desktop\tarea.txt"; // EL PATH DEL ARCHIVO VARIA EN CADA EQUIPO
+            if (!File.Exists(path))
+            {
+                string inicio = "Datos de la asignatura:" + Environment.NewLine;
+                File.WriteAllText(path, inicio);
+            }
+            var ne = Environment.NewLine;
+            string appendt = ne + "La asignatura es: " + asignatura + ne + "El aula es: " + aula + ne + "Los creditos de la asignatura son: " + creditos + ne + "El instructr asignatura es: " + instructor + ne;
+            File.AppendAllText(path, appendt);
+
+            try 
+            {
+                Console.WriteLine("Los datos del archivo son: \n");
+                string readText = File.ReadAllText(path);
+                Console.WriteLine(readText);
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("La ubicacion del archivo no fue encontrada.");
+                Console.WriteLine("Ingrese la ubicacion correcta.");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            evaluacion lol = new evaluacion();
+            lol.mostrarDatos();
+        }
+    }
+}
